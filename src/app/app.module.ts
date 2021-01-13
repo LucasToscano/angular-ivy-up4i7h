632f36f982a3650607110ee5aa06dd1b60588918
LucasToscano <lucastlandgraf73@gmail.com>
2021-01-12 22:32:04 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, HelloComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
