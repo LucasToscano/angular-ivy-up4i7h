25e523c9a555fa1329563bd9a5813cdc201d375f
LucasToscano <lucastlandgraf73@gmail.com>
2021-01-12 23:02:09 -0300

    2 files modified

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
+  selector: "app-servers",
   templateUrl: `./servers.component.html`,
-  styleUrls: ['./servers.component.css']
+  styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
 
-  constructor() { 
-    setTimeout(() => {},2000)
+  constructor() {
+    setTimeout(() => {}, 2000);
     {
       this.allowNewServer = true;
     }
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
