f151a9074315245bceffce75c377f4b354f3c24d
LucasToscano <lucastlandgraf73@gmail.com>
2021-01-15 02:42:51 -0300

    3 files modified

--- src/app/exercicio3/exercicio3.component.ts
+++ src/app/exercicio3/exercicio3.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-exercicio3',
-  templateUrl: './exercicio3.component.html',
-  styleUrls: ['./exercicio3.component.css']
+  selector: "app-exercicio3",
+  templateUrl: "./exercicio3.component.html",
+  styleUrls: ["./exercicio3.component.css"]
 })
 export class Exercicio3Component implements OnInit {
+  contador: number = 0;
+  valor = [];
 
-contador : number = 0;
-valor = [];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClick() {
+    this.contador++;
+    this.valor.push(this.contador);
+    this.valor.length > 0;
   }
-onClick()
-{
-  this.contador ++;
-  this.valor.push(this.contador)
-  this.valor.length >= 5
-}
+
+  stylesBackGround(count) {
+    if (count <= 5) {
+      return "white";
+    } else {
+      return "blue";
+    }
+  }
+
+  stylesText(count) {
+    if (count <= 5) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
